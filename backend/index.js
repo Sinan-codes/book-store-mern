50a42cdc4b51af078568defb4c74654ac88c1a6a
Mohamed Kassim Sinan <mohamed.kassim.sinan@gmail.com>
2025-03-22 14:01:31 +0400

    applied cors policy

--- backend/index.js
+++ backend/index.js
@@ -1,58 +1,30 @@
 import express, { request, response } from "express";
 import mongoose from "mongoose";
 import { PORT, mongoDBURL } from "./config.js";
-
 import { Book } from "./models/bookModel.js";
+import booksRoute from "./routes/booksRoute.js";
+import cors from 'cors';
 
 const app = express();
 
 app.use(express.json());
+
+// app.use(cors());
+
+app.use(
+    cors({
+        origin: "http://localhost:3000",
+        methods: ["GET", "POST", "PUT", "DELETE"],
+        allowedHeaders: ["Content-Type"],
+    })
+)
 
 app.get('/', (req, res) => {
     console.log(req);
     return res.status(234).send("Welcome to MERN Stack");
 });
 
-app.post('/books', async (req, res) => {
-    try {
-        if (
-            !req.body.title ||
-            !req.body.author ||
-            !req.body.publishYear
-        ) {
-            return res.status(400).send({ message: "All fields are required: title, author, publishYear" });   
-        };
-
-        
-
-        const newBook = {
-            title: req.body.title,
-            author: req.body.author,
-            publishYear: req.body.publishYear,
-        }
-
-        const book = await Book.create(newBook);
-
-        return res.status(201).send(book);
-        
-    } catch (error) {
-        console.log(error);
-        res.status(500).send({ message: error.message });
-    }
-});
-
-app.get('/books', async (req, res) => {
-    try {
-        const books = await Book.find({});
-
-        return res.status(200).send(books);
-    } catch (error) {
-        console.log(error.message);
-        res.status(500).send({ message: error.message });
-    }
-});
-
-
+app.use('/books', booksRoute);
 
 mongoose
     .connect(mongoDBURL)
